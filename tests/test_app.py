77cb97818b4e898aa2afcb0fb52ae77de44044d2
Sahil Ahmad Sheikh <sheikhsahil95965@gmail.com>
2026-06-25 22:44:40 +0530

    Last commit

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,6 +1,10 @@
+import sys, os
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
 from app import app
 
 def test_home():
-    response = app.test_client().get('/')
+    response = app.test_client().get("/")
     assert response.status_code == 200
     assert response.data == b"Hello, World!"
+
